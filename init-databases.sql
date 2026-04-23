51b59220be0b3d8ef9031f0f04f8515f947c9306
hkarpinen <karpinenh@gmail.com>
2026-04-22 23:28:34 -0700

    chore: initial commit

---
+++ init-databases.sql
@@ -0,0 +1,12 @@
+-- init-databases.sql
+-- Creates all three application databases on first docker compose up.
+-- Mounted into /docker-entrypoint-initdb.d/ in the postgres container.
+
+SELECT 'CREATE DATABASE identity_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'identity_db')\gexec
+
+SELECT 'CREATE DATABASE forum_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'forum_db')\gexec
+
+SELECT 'CREATE DATABASE bills_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'bills_db')\gexec
